f9b38e437de019d041bc94865aa04b52c051536d
ubuntu <ubuntu@ubuntu-20045lts-base.myguest.virtualbox.org>
2022-12-04 14:12:05 -0500

    compute engine startup script with mods v2

--- ComputeEngine/Video71_StartupScript.sh
+++ ComputeEngine/Video71_StartupScript.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 "--------------------------------------------------" >> ~/startup.log 
 date +"%s" >> ~/startup.log
-sudo apt update & sudo apt -y upgrade
+sudo apt update
+sudo apt -y upgrade
 apt -y install apache2
 echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
 date +"%s" >> ~/startup.log
-"--------------------------------------------------" >> ~/startup.log 
+"--------------------------------------------------" >> ~/startup.log
